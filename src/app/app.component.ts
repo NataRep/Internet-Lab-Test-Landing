86422ee2787eeb807b7cb4a1e1582022bb5752c1
Natalia Repkina <pn.88@mail.ru>
2024-10-27 11:15:43 +0300

    feat: add sixth block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { SecondComponent } from './components/sections/second/second.component';
 import { ThirdComponent } from './components/sections/third/third.component';
 import { FourthComponent } from './components/sections/fourth/fourth.component';
 import { FifthComponent } from './components/sections/fifth/fifth.component';
+import { SixthComponent } from './components/sections/sixth/sixth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, SecondComponent, ThirdComponent, FourthComponent, FifthComponent],
+  imports: [HeaderComponent, SecondComponent, ThirdComponent, FourthComponent, FifthComponent, SixthComponent],
   providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
